3643eecbe34ffbc5108c9d99c902760b6a55b2f5
Marek Dziezak <marekd5@hotmail.com>
2021-02-20 19:31:35 +0000

    Messages corrected and attempts moved to higher position in the code.

--- game.py
+++ game.py
@@ -15,14 +15,14 @@ print('You have 5 chances')
 print()
 while attempts < attempt_limit:
     guess = int(input('What is your guess? '))
+    attempts += 1
     if guess == mm_count:
-        print('Great! You won!')
+        print(f'Great! You won! It took you {attempts} tries.')
         break
     elif guess < mm_count:
-        print('Too low')
+        print(f'The {guess} is too LOW')
     elif guess > mm_count:
-        print('Too high')
+        print(f'The {guess} is too HIGH')
     else:
         print('It is not a value between 1 and 100')
-    attempts += 1
 print('Bye!')
